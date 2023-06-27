63abcaa8ebdcee376e8c664b179a699161a514b9
Felipe Mahia <felipemahia95@gmail.com>
2023-06-27 15:58:56 -0300

    primer commit

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import { ShoppingCart } from "@mui/icons-material";
+import React from "react";
+
+export const CartWidget = () => { 
+    return(
+        <>
+        <ShoppingCart></ShoppingCart>
+        </>
+    )
+}
+
+export default CartWidget 
---
+++ src/components/NavBar/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+//import LogoVL from "../../assets/LOGO.png"
+import "../NavBar/Logo.css"
+
+export function Logo () {
+    return(
+        <div className="logoContainer">
+          <h1>Logo</h1>
+        </div>
+    )
+}
